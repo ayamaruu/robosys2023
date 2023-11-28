67f8c5437fdafd07046808f67ea55ff8b3744c4c
Ayaka Murakoshi <s22c1123uf@s.chibakoudai.jp>
2023-11-28 14:46:46 +0900

    test2

--- test.bash
+++ test.bash
@@ -1,5 +1,23 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Ayaka Murakoshi
+# SPDX-Linense-Identifier: BSD-3-Clause
+ng () {
+	echo NG at Line $1
+	res=1
+}
 
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15.0 ]
+[ "${out}" = 14.0 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
+* このソフトウェアパッケージは、3条項BSDライセンスの下、再頒布および使用が許可されます.
+* このパッケージのコードは下記のスライド(CC-BY-SA 4.0 by Ryuichi Ueda)のものを,本人の許可を得て自身の著作としたものです.
+* [ryuichiueda/my_slides robosys_2022](http://githb.com/ryuichiueda/my_slides/tree/master/robosys_2022)
+* © 2023 Ayaka Murakoshi
+
