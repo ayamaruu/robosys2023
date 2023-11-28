382b8a1aeb5c414f5680d1e88adc2cd6d9f2cdc3
Ayaka Murakoshi <s22c1123uf@s.chibakoudai.jp>
2023-11-28 15:32:34 +0900

    test3

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Ayaka Murakoshi
 # SPDX-Linense-Identifier: BSD-3-Clause
 ng () {
@@ -10,8 +10,12 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
 
-[ "${out}" = 14.0 ] || ng ${LINENO}
+### STRANGE INPUT ###
+out=$(seq あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
