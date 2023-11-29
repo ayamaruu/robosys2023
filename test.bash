0c396c4b40ec3c77504e6c03f7aa224e5c31ca6a
Ayaka Murakoshi <s22c1123uf@s.chibakoudai.jp>
2023-11-29 14:05:18 +0900

    test3

--- test.bash
+++ test.bash
@@ -36,6 +36,7 @@ out=$(seq 10 | ./plus)
 [ "$res" = 0 ] && echo OK
 exit $res
 
+
 * このソフトウェアパッケージは、3条項BSDライセンスの下、再頒布および使用が許可されます.
 * このパッケージのコードは下記のスライド(CC-BY-SA 4.0 by Ryuichi Ueda)のものを,本人の許可を得て自身の著作としたものです.
 * [ryuichiueda/my_slides robosys_2022](http://githb.com/ryuichiueda/my_slides/tree/master/robosys_2022)
