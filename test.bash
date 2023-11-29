6efbfd542b4e4ba09d12bce84db499d258b7d67b
Ayaka Murakoshi <s22c1123uf@s.chibakoudai.jp>
2023-11-29 23:28:44 +0900

    test8

--- test.bash
+++ test.bash
@@ -36,18 +36,10 @@ out=$(seq 5 | ./minus)
 out=$(seq 10 | ./minus)
 [ "${out}" = -55 ] || ng ${LINENO}
 
-
-### STRANGE INPUT ###
-#out=$(seq あ | ./plus)
-#[ "$?" = 1 ]      || ng ${LINENO}
-#[ "${out}" = "" ] || ng ${LINENO}
-
 [ "$res" = 0 ] && echo OK
 exit $res
 
 
 * このソフトウェアパッケージは、3条項BSDライセンスの下、再頒布および使用が許可されます.
-* このパッケージのコードは下記のスライド(CC-BY-SA 4.0 by Ryuichi Ueda)のものを,本人の許可を得て自身の著作としたものです.
-* [ryuichiueda/my_slides robosys_2022](http://githb.com/ryuichiueda/my_slides/tree/master/robosys_2022)
 * © 2023 Ayaka Murakoshi
 
